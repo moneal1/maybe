5a14826ab315ec2a1d69bfcf9dc678197b82204d
Mike O'Neal <MikeONeal18@gmail.com>
2019-05-16 03:47:21 +0000

    some commit message

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,5 +8,21 @@ end
 
       @quote = Quote.new
   end
+
+
+  def create
+    Quote.create(quote_params)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+
+    params.require(:quote).permit(:saying, :author)
+
+  end
+
 end
 
